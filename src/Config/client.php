1e62910ed26773a20b8d23ac4a687bc0616246a4
Moses <59233368+jewishmoses@users.noreply.github.com>
2020-09-15 04:09:01 +0300

    Update client.php

--- src/Config/client.php
+++ src/Config/client.php
@@ -15,7 +15,7 @@ return [
      * File cache configs.
      */
     'file' => [
-        'dir' => \dirname(__DIR__, 2) . DIRECTORY_SEPARATOR . '.cache' . DIRECTORY_SEPARATOR,
+        'dir' => \dirname(__DIR__, 5) . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR . 'framework' . DIRECTORY_SEPARATOR . 'cache' . DIRECTORY_SEPARATOR,
         'name' => 'tus_php.client.cache',
     ],
 ];
